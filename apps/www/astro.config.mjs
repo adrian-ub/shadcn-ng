5961c3d6702e7640b4ae228aa8b4b31e49e76bd3
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-07-23 18:48:22 -0500

    feat: add site URL to astro config

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ui.adrianub.dev",
   integrations: [
     // angular(),
     starlight({
