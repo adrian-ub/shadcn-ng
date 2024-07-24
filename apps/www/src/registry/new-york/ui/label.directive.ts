52bfed8b38434e85127bc0be0fc3996b78563150
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-07-23 19:04:02 -0500

    feat: add label component

---
+++ apps/www/src/registry/new-york/ui/label.directive.ts
@@ -0,0 +1,28 @@
+import { computed, Directive, input } from "@angular/core";
+
+import { RdxLabelRootDirective } from "@radix-ng/primitives/label";
+import { cva } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const labelVariants = cva(
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+);
+
+@Directive({
+  selector: "[ubLabel]",
+  standalone: true,
+  hostDirectives: [
+    {
+      directive: RdxLabelRootDirective,
+      inputs: ["htmlFor"],
+    },
+  ],
+  host: {
+    "[class]": "computedClass()",
+  },
+})
+export class UbLabelDirective {
+  readonly class = input<string>("");
+  protected computedClass = computed(() => cn(labelVariants(), this.class()));
+}
