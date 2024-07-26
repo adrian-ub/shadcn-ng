d34f859f6be24481430c1c1db0ab8fe2e39ea5c3
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-07-26 12:11:40 -0500

    feat: initial layout

---
+++ apps/www/src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "shadcn-ng",
+  url: "https://ui.adrianub.dev",
+  ogImage: "https://ui.adrianub.dev/og.jpg",
+  description:
+    "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+  links: {
+    twitter: "https://twitter.com/adrianub",
+    github: "https://github.com/adrian-ub/shadcn-ng",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
---
+++ apps/www/src/schemas/badge.ts
@@ -0,0 +1,31 @@
+import { z } from "astro/zod";
+
+const badgeSchema = () =>
+  z.object({
+    variant: z
+      .enum(["note", "danger", "success", "caution", "tip", "default"])
+      .default("default"),
+    text: z.string(),
+    class: z.string().optional(),
+  });
+
+export const BadgeComponentSchema = badgeSchema()
+  .extend({
+    size: z.enum(["small", "medium", "large"]).default("small"),
+  })
+  .passthrough();
+
+export type BadgeComponentProps = z.input<typeof BadgeComponentSchema>;
+
+export const BadgeConfigSchema = () =>
+  z
+    .union([z.string(), badgeSchema()])
+    .transform((badge) => {
+      if (typeof badge === "string") {
+        return { variant: "default" as const, text: badge };
+      }
+      return badge;
+    })
+    .optional();
+
+export type Badge = z.output<ReturnType<typeof badgeSchema>>;
