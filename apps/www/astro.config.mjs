713525a7ef8765b55dc07bfc3ceafa59ce8240f0
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-07-26 19:15:21 -0500

    feat: add component preview

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -2,48 +2,61 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 import angular from "@analogjs/astro-angular";
-
 import { siteConfig } from "./src/config/site";
+import theme from "./src/lib/highlighter-theme.json";
+
+import simpleStackQuery from "simple-stack-query";
 
 // https://astro.build/config
 export default defineConfig({
   site: siteConfig.url,
-  integrations: [
-    angular(),
-    starlight({
-      title: siteConfig.name,
-      logo: {
-        dark: "./src/assets/logo-dark.svg",
-        light: "./src/assets/logo-light.svg",
-      },
-      favicon: "./src/assets/logo-dark.svg",
-      social: {
-        github: siteConfig.links.github,
-        "x.com": siteConfig.links.twitter,
-      },
-      sidebar: [
-        {
-          label: "Getting Started",
-          autogenerate: {
-            directory: "docs",
-          },
-        },
-      ],
-      customCss: ["./src/tailwind.css"],
-      components: {
-        Header: "./src/components/starlight/header/Header.astro",
-        PageFrame: "./src/components/starlight/PageFrame.astro",
-        SiteTitle: "./src/components/starlight/SiteTitle.astro",
-        SocialIcons: "./src/components/starlight/SocialIcons.astro",
-        Search: "./src/components/starlight/Search.astro",
-        Hero: "./src/components/starlight/Hero.astro",
-        ContentPanel: "./src/components/starlight/ContentPanel.astro",
-        PageTitle: "./src/components/starlight/PageTitle.astro",
-        MarkdownContent: "./src/components/starlight/MarkdownContent.astro",
-        TwoColumnContent: "./src/components/starlight/TwoColumnContent.astro",
-        Sidebar: "./src/components/starlight/Sidebar.astro",
-      },
-    }),
-    tailwind(),
-  ],
+  integrations: [starlight({
+    title: siteConfig.name,
+    expressiveCode: {
+      themes: [theme]
+    },
+    logo: {
+      dark: "./src/assets/logo-dark.svg",
+      light: "./src/assets/logo-light.svg"
+    },
+    favicon: "./src/assets/logo-dark.svg",
+    social: {
+      github: siteConfig.links.github,
+      "x.com": siteConfig.links.twitter
+    },
+    sidebar: [{
+      label: "Getting Started",
+      items: [{
+        label: "Introduction",
+        slug: "docs"
+      }]
+    }, {
+      label: "Components",
+      autogenerate: {
+        directory: "docs/components"
+      }
+    }],
+    customCss: ["./src/tailwind.css"],
+    components: {
+      Header: "./src/components/starlight/header/Header.astro",
+      PageFrame: "./src/components/starlight/PageFrame.astro",
+      SiteTitle: "./src/components/starlight/SiteTitle.astro",
+      SocialIcons: "./src/components/starlight/SocialIcons.astro",
+      Search: "./src/components/starlight/Search.astro",
+      Hero: "./src/components/starlight/Hero.astro",
+      ContentPanel: "./src/components/starlight/ContentPanel.astro",
+      PageTitle: "./src/components/starlight/PageTitle.astro",
+      MarkdownContent: "./src/components/starlight/MarkdownContent.astro",
+      TwoColumnContent: "./src/components/starlight/TwoColumnContent.astro",
+      Sidebar: "./src/components/starlight/Sidebar.astro"
+    }
+  }), tailwind(), angular(), simpleStackQuery()],
+  vite: {
+    optimizeDeps: {
+      include: ["@radix-ng/primitives", "@angular/common", "@angular/core", "@angular/cdk", "@ng-icons/core", "@ng-icons/lucide"]
+    },
+    ssr: {
+      noExternal: ["@radix-ng/primitives", "@angular/cdk", "@ng-icons/core", "@ng-icons/lucide"]
+    }
+  }
 });
---
+++ apps/www/src/registry/new-york/example/alert-demo.ts
@@ -0,0 +1,32 @@
+import { Component } from "@angular/core";
+import { NgIconComponent, provideIcons } from "@ng-icons/core";
+import { lucideTerminal } from "@ng-icons/lucide";
+
+import {
+  UbAlertDirective,
+  UbAlertTitleDirective,
+  UbAlertDescriptionDirective,
+} from "@/registry/new-york/ui/alert.directive";
+
+@Component({
+  standalone: true,
+  imports: [
+    NgIconComponent,
+    UbAlertDirective,
+    UbAlertTitleDirective,
+    UbAlertDescriptionDirective,
+  ],
+  viewProviders: [provideIcons({ lucideTerminal })],
+  template: `
+    <div ubAlert>
+      <ng-icon name="lucideTerminal" class="h-4 w-4" />
+      <h5 ubAlertTitle>Heads up!</h5>
+      <div ubAlertDescription>
+        You can add components and dependencies to your app using the cli.
+      </div>
+    </div>
+  `,
+})
+export class AlertDemoComponent {}
+
+export default AlertDemoComponent;
