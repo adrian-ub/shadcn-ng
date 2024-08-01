71aaca82143b3ce07e85071866b983c2c20e3d9a
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-07-31 21:37:49 -0500

    feat: update title delimiter in Astro configuration

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: siteConfig.name,
+      titleDelimiter: "-",
       expressiveCode: {
         themes: [theme],
         styleOverrides: {
