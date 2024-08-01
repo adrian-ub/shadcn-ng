3451cf3de35d40dc0ec5414864c0c0b1dd83223d
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-07-31 21:30:53 -0500

    feat: add initial typography

---
+++ apps/www/src/registry/new-york/example/typography-h1.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  template: `
+    <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      Taxing Laughter: The Joke Tax Chronicles
+    </h1>
+  `,
+})
+export class TypographyH1Component {}
+
+export default TypographyH1Component;
