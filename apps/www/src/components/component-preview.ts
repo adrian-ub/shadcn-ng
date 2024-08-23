daa2073cc26a51b448c2092104ef115bd39ca0fa
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-08-22 20:42:49 -0500

    feat: add examples to the registry and update ComponentPreview

--- apps/www/src/components/component-preview.ts
+++ apps/www/src/components/component-preview.ts
@@ -1,17 +1,29 @@
 import { Component, computed, input } from '@angular/core';
 import { AsyncPipe, NgComponentOutlet } from '@angular/common';
 
+import { examples } from '@/__registry__/examples'
+
 @Component({
     standalone: true,
     imports: [NgComponentOutlet, AsyncPipe],
     template: `
-    <ng-container *ngComponentOutlet="component() | async"  />
+    @let componentRender = this.component() | async;
+
+    @if(!componentRender || !componentRender.default) {
+        <div>Loading...</div>
+    } @else {
+        <ng-container *ngComponentOutlet="componentRender.default"  />
+    }
     `
 })
 export class ComponentPeviewComponent {
-    path = input.required<string>();
+    styleName = input<string>();
+    nameExample = input<string>();
+    examples = examples;
 
-    protected component = computed(async () => {
-        return (await import(/* @vite-ignore */ this.path())).default;
+    component = computed(async () => {
+        if (!this.styleName() || !this.nameExample()) return null;
+
+        return await examples[this.styleName()!][this.nameExample()!].component();
     });
 }
