f391f19a458d2a29ef0b18bf4b621a1436ecc964
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-08-27 19:36:29 -0500

    feat: add accordion

--- apps/www/src/registry/ui.ts
+++ apps/www/src/registry/ui.ts
@@ -1,62 +1,68 @@
 import type { Registry } from "./schema";
 
 export const ui: Registry = [
-  {
-    name: "alert",
-    type: "components:ui",
-    files: ["ui/alert.directive.ts"],
-  },
-  {
-    name: "badge",
-    type: "components:ui",
-    files: ["ui/badge.directive.ts"],
-  },
-  {
-    name: "breadcrumb",
-    type: "components:ui",
-    files: ["ui/breadcrumb.directive.ts"],
-    dependencies: ["@ng-icons/core", "@ng-icons/radix-icons"],
-  },
-  {
-    name: "button",
-    type: "components:ui",
-    files: ["ui/button.directive.ts"],
-  },
-  {
-    name: "card",
-    type: "components:ui",
-    files: ["ui/card.directive.ts"],
-  },
-  {
-    name: "input",
-    type: "components:ui",
-    files: ["ui/input.directive.ts"],
-  },
-  {
-    name: "label",
-    type: "components:ui",
-    dependencies: ["@radix-ng/primitives"],
-    files: ["ui/label.directive.ts"],
-  },
-  {
-    name: "separator",
-    type: "components:ui",
-    files: ["ui/separator.directive.ts"],
-  },
-  {
-    name: "skeleton",
-    type: "components:ui",
-    files: ["ui/skeleton.directive.ts"],
-  },
-  {
-    name: "table",
-    type: "components:ui",
-    files: ["ui/table.directive.ts"],
-  },
-  {
-    name: "tabs",
-    type: "components:ui",
-    dependencies: ["@angular/cdk", "@radix-ng/primitives"],
-    files: ["ui/tabs.directive.ts"],
-  },
+    {
+        name: "accordion",
+        type: "components:ui",
+        dependencies: ["@ng-icons/core", "@ng-icons/lucide"],
+        files: ["ui/accordion.directive.ts"],
+    },
+    {
+        name: "alert",
+        type: "components:ui",
+        files: ["ui/alert.directive.ts"],
+    },
+    {
+        name: "badge",
+        type: "components:ui",
+        files: ["ui/badge.directive.ts"],
+    },
+    {
+        name: "breadcrumb",
+        type: "components:ui",
+        files: ["ui/breadcrumb.directive.ts"],
+        dependencies: ["@ng-icons/core", "@ng-icons/radix-icons"],
+    },
+    {
+        name: "button",
+        type: "components:ui",
+        files: ["ui/button.directive.ts"],
+    },
+    {
+        name: "card",
+        type: "components:ui",
+        files: ["ui/card.directive.ts"],
+    },
+    {
+        name: "input",
+        type: "components:ui",
+        files: ["ui/input.directive.ts"],
+    },
+    {
+        name: "label",
+        type: "components:ui",
+        dependencies: ["@radix-ng/primitives"],
+        files: ["ui/label.directive.ts"],
+    },
+    {
+        name: "separator",
+        type: "components:ui",
+        files: ["ui/separator.directive.ts"],
+    },
+    {
+        name: "skeleton",
+        type: "components:ui",
+        files: ["ui/skeleton.directive.ts"],
+    },
+    {
+        name: "table",
+        type: "components:ui",
+        files: ["ui/table.directive.ts"],
+    },
+    {
+        name: "tabs",
+        type: "components:ui",
+        dependencies: ["@angular/cdk", "@radix-ng/primitives"],
+        files: ["ui/tabs.directive.ts"],
+    },
 ];
