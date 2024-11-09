d869ab9596132ad7c96f667027f41edfbbeead7b
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-11-09 18:00:25 -0500

    docs: remove width

--- docs/src/registry/default/example/aspect-ratio-demo.ts
+++ docs/src/registry/default/example/aspect-ratio-demo.ts
@@ -16,19 +16,17 @@ import { Component } from '@angular/core'
     },
   ],
   template: `
-  <div class="w-96">
-    <div ubAspectRatio [ratio]="16 / 9" class="bg-muted">
-        <img
-          ngSrc="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-          alt="Photo by Drew Beamer"
-          fill
-          class="h-full w-full rounded-md object-cover"
-          ngSrcset="640w, 750w, 828w, 1080w, 1200w, 1920w, 2048w, 3840w"
-          sizes="100vw"
-          style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;"
-          priority
-        />
-    </div>
+  <div ubAspectRatio [ratio]="16 / 9" class="bg-muted">
+      <img
+        ngSrc="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+        alt="Photo by Drew Beamer"
+        fill
+        class="h-full w-full rounded-md object-cover"
+        ngSrcset="640w, 750w, 828w, 1080w, 1200w, 1920w, 2048w, 3840w"
+        sizes="100vw"
+        style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;"
+        priority
+      />
   </div>
   `,
 })
