40521f721cc258366357924c17764e69fdedd311
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2025-04-14 18:19:21 -0500

    chore: ignore files

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,10 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   astro: true,
+  pnpm: true,
+  ignores: [
+    'apps/www/src/components/mdx/pre.astro',
+    'apps/www/src/components/CodeBlockCommand.astro',
+    '**/.astro',
+  ],
 })
