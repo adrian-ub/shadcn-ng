0e67c17c42d99077a9ced78446d71046ae2bccbf
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2025-06-16 20:59:20 -0500

    docs: add fonts

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight'
 import tailwindcss from '@tailwindcss/vite'
 
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,10 +23,34 @@ export default defineConfig({
       customCss: ['./src/styles/global.css'],
       components: {
         PageFrame: './src/components/starlight/PageFrame.astro',
+        Head: './src/components/starlight/Head.astro',
       },
     }),
   ],
   vite: {
     plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Geist',
+        cssVariable: '--font-sans',
+        subsets: ['latin'],
+      },
+      {
+        provider: fontProviders.google(),
+        name: 'Geist Mono',
+        cssVariable: '--font-mono',
+        subsets: ['latin'],
+        weights: ['400'],
+      },
+      {
+        provider: fontProviders.google(),
+        name: 'Inter',
+        cssVariable: '--font-inter',
+        subsets: ['latin'],
+      },
+    ],
   },
 })
