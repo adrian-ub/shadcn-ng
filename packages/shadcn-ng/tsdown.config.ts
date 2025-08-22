fb7f749ed21e95d5ae20cff753929ed2bcbb09b8
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2025-08-21 21:26:11 -0500

    chore: add entrypoints and script

--- packages/shadcn-ng/tsdown.config.ts
+++ packages/shadcn-ng/tsdown.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig([
   {
-    entry: ['./src/index.ts'],
+    entry: [
+      'src/index.ts',
+      'src/registry/index.ts',
+      'src/registry/schema.ts',
+      'src/mcp/index.ts',
+    ],
     platform: 'node',
     dts: true,
+    format: ['esm'],
+    sourcemap: true,
+    minify: true,
   },
 ])
