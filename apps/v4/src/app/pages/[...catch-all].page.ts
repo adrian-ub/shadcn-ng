78656165dfb1c3a466376981f08fcbe70018c870
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2025-09-08 08:06:43 -0500

    feat: add PageNotFound component

---
+++ apps/v4/src/app/pages/[...catch-all].page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  template: `page not found`,
+})
+export default class PageNotFoundPageComponent { }
