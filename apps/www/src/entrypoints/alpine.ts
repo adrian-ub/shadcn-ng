cbd6cb77115638ad0d6c82c1d3f331bc00e30c6b
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2026-09-11 19:44:33 -0500

    reset

--- apps/www/src/entrypoints/alpine.ts
+++
@@ -1,6 +0,0 @@
-import type { Alpine } from 'alpinejs'
-import focus from '@alpinejs/focus'
-
-export default (Alpine: Alpine): void => {
-  Alpine.plugin(focus)
-}
--- apps/www/src/pages/r/styles/index.json.ts
+++
@@ -1,5 +0,0 @@
-import { styles } from '~/registry/registry-styles'
-
-export async function GET(): Promise<Response> {
-  return new Response(JSON.stringify(styles, null, 2))
-}
